fd0a66d441623c630b72e77e4f5aa5e643dcfac4
Lucas Peres <82038034+lmperess@users.noreply.github.com>
2023-06-17 02:31:31 -0300

    Update comparador.vhd

--- ProjetoFinal-22.1/comparador.vhd
+++ ProjetoFinal-22.1/comparador.vhd
@@ -4,11 +4,13 @@ use IEEE.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
 entity comparador is port(
-in0, in1: in  std_logic_vector(7 downto 0);
-S: out std_logic);
+         in0, in1: in  std_logic_vector(7 downto 0);
+         S: out std_logic
+         );
 end comparador;
 
 architecture comp of comparador is 
+         
 begin
 
 S <= '1' when (in0 = in1) else
