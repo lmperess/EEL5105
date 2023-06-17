c597225af945a5a5022ffcfe42db3a21114ccb91
Lucas Peres <82038034+lmperess@users.noreply.github.com>
2023-06-17 02:44:27 -0300

    Update ButtonPlay.vhd

--- ProjetoFinal-22.1/ButtonPlay.vhd
+++ ProjetoFinal-22.1/ButtonPlay.vhd
@@ -2,11 +2,10 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity ButtonPlay is port(
-
     KEY1, KEY0: in  std_logic;
     Reset, clk: in  std_Logic;
-    BTN1, BTN0: out std_logic); 
-    
+    BTN1, BTN0: out std_logic
+    ); 
 end ButtonPlay;
 
 architecture arc_btn of ButtonPlay is 
@@ -19,44 +18,36 @@ begin
     
     process (clk, Reset) 
 	begin
-		
 		if Reset = '1' then
 			BTN1_state <= EsperaApertar;
 			BTN0_state <= EsperaApertar;
-			
 		elsif clk'event and clk = '1' then
 			BTN1_state <= BTN1_next;
 			BTN0_state <= BTN0_next;
 		end if;
-	end process;
+    end process;
 	
-	
-	process (key1, BTN1_state)
+    process (key1, BTN1_state)
 	begin
 		case BTN1_state is
 			when EsperaApertar =>
 				BTN1 <= '0';
-				if key1 = '0' then BTN1_next <= Apertado; else BTN1_next <= EsperaApertar; end if;
-				
+				if key1 = '0' then BTN1_next <= Apertado; else BTN1_next <= EsperaApertar; end if;	
 			when Apertado =>
 				BTN1 <= '1';
-				BTN1_next <= Apertado;
-			
+				BTN1_next <= Apertado;	
 		end case;		
-	end process;
+    end process;
 	
 	
-	process (key0, BTN0_state)
+    process (key0, BTN0_state)
 	begin
 		case BTN0_state is
 			when EsperaApertar =>
 				BTN0 <= '0';
 				if key0 = '0' then BTN0_next <= Apertado; else BTN0_next <= EsperaApertar; end if;
-				
-			when Apertado =>
-				BTN0 <= '1';
-				BTN0_next <= Apertado;
-			
+			when Apertado => BTN0 <= '1';
+				         BTN0_next <= Apertado;
 		end case;		
-	end process;
+     end process;
 end arc_btn;
