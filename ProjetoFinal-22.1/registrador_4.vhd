5a7af4ad53ab0b549eb4542b10900a26195f6090
Lucas Peres <82038034+lmperess@users.noreply.github.com>
2023-06-17 02:16:47 -0300

    Update registrador_4.vhd

--- ProjetoFinal-22.1/registrador_4.vhd
+++ ProjetoFinal-22.1/registrador_4.vhd
@@ -1,25 +1,25 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity registrador_4 is port (
-CLK, RST, enable: in std_logic;
-D: in std_logic_vector(3 downto 0);
-Q: out std_logic_vector(3 downto 0)
-);
-end registrador_4;
+entity registrador_4bits is port (
+  CLK, RST, enable: in std_logic;
+  D: in std_logic_vector(3 downto 0);
+  Q: out std_logic_vector(3 downto 0)
+  );
+end registrador_4bits;
 
-architecture rgt_4 of registrador_4 is
+architecture rgt_4bits of registrador_4bits is
 begin
 
 process(CLK, D, RST, enable)
-begin
-if RST = '1' then
-Q <= "0000";
-elsif (CLK'event and CLK = '1') then
-if enable = '1' then 
-Q <= D;
-end if;
-end if;
+  begin
+  if RST = '1' then
+    Q <= "0000";
+  elsif (CLK'event and CLK = '1') then
+    if enable = '1' then 
+      Q <= D;
+    end if;
+  end if;
 end process;
 
-end rgt_4;
+end rgt_4bits;
