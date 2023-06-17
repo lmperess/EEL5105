7d3e52b175b8d2711fa736b90738b2079e5bd84e
Lucas Peres <82038034+lmperess@users.noreply.github.com>
2023-06-17 03:00:17 -0300

    Update Mux2_1x8.vhd

--- ProjetoFinal-22.1/Mux2_1x8.vhd
+++ ProjetoFinal-22.1/Mux2_1x8.vhd
@@ -2,14 +2,15 @@ library ieee;
 use ieee.std_Logic_1164.all;
 
 entity Mux2_1x8 is port(
-
     S     : in  std_logic;
     L0, L1: in  std_logic_vector(7 downto 0);
-    D     : out std_logic_vector(7 downto 0));
-    
+    D     : out std_logic_vector(7 downto 0)
+    );
 end Mux2_1x8;
 
-architecture arcmux4_1 of Mux2_1x8 is begin
+architecture arcmux4_1 of Mux2_1x8 is 
+    
+begin
 
     D <= L0 when S = '0' else
          L1;
